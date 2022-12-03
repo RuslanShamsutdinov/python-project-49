bfd7641eee6072f0f77fc090192941106a3d15fa
Ruslan Shamsutdinov <nexus191@yandex.ru>
2022-12-03 15:48:25 +0500

    mentor check2

--- brain_games/games/brain_gcd.py
+++ brain_games/games/brain_gcd.py
@@ -8,5 +8,5 @@ def get_question_and_correct_answer():
     random_number1 = randint(1, 10)
     random_number2 = randint(1, 10)
     Question = f'{random_number1} {random_number2}'
-    correct_answer = gcd(random_number1, random_number2)
+    correct_answer = str(gcd(random_number1, random_number2))
     return correct_answer, Question
