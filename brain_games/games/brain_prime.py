bf881565cca78b420f7318050ddb94460c4027bd
Ruslan Shamsutdinov <nexus191@yandex.ru>
2022-12-10 02:11:57 +0500

    mentor check3

--- brain_games/games/brain_prime.py
+++ brain_games/games/brain_prime.py
@@ -4,12 +4,18 @@ ANNOTATION = """Answer "yes" if given number is prime. Otherwise answer "no"."""
 
 
 def is_prime(random_number):
-    for i in range(2, random_number // 2 + 1):
-        if random_number in [0, 1] or random_number % i == 0:
-            return False
-        else:
-            return True
-
+    divider_count = 0
+    if random_number == 2:
+        return True
+    if random_number in [0, 1]:
+        return False
+    for i in range(1, random_number // 2 + 1):
+        if random_number % i == 0:
+            divider_count += 1
+    if divider_count > 1:
+        return False
+    else:
+        return True
 
 def get_question_and_correct_answer():
     correct_answer = 'no'
@@ -17,3 +23,4 @@ def get_question_and_correct_answer():
     if is_prime(question):
         correct_answer = 'yes'
     return correct_answer, question
+
